f80dd86cb23ab73ca1f8e4384e9c707fb6aad5d6
AnthonyTB <Anthonylol@protonmail.com>
2019-11-01 14:05:22 -0700

    finished

--- app.js
+++ app.js
@@ -5,49 +5,43 @@
 function handleParkInfo() {
   $('#dataEntry').on('submit', e => {
     e.preventDefault();
-    let parkName = $('.entryPoint').val().toLowerCase();
-    apiCallPark(parkName);
+    const apiUrl = 'https://api.nps.gov/api/v1/parks';
+    const stateArr = $('.entryPoint').val().split(',');
+    const maxResults = $('.max-results').val();
+    const apiKey = '0nxZI9hF2LSNbDho29iIJk8FXwUmHWzH3HXGrizh';
+    apiCallPark(apiUrl, stateArr, maxResults, apiKey);
   });
 }
 
-function apiCallPark(parkName) {
-  fetch('https://developer.nps.gov/api/v1/parks?parkCode=acad&api_key=0nxZI9hF2LSNbDho29iIJk8FXwUmHWzH3HXGrizh')
+function formatParams(params) {
+  const queries = Object.keys(params).map(key => `${[ encodeURIComponent(key) ]}=${encodeURIComponent(params[key])}`);
+  return queries.join('&');
+}
+
+function apiCallPark(apiUrl, stateArr, maxResults, apiKey) {
+  const params = {
+    stateCode: stateArr,
+    limit: maxResults
+  };
+
+  const queryString = formatParams(params);
+  const url = apiUrl + '?' + queryString + '&api_key=' + apiKey;
+
+  fetch(url)
     .then(response => response.json())
-    .then(responseJson => displayResults(responseJson))
+    .then(responseJson => createTemplate(responseJson, maxResults))
     .catch(error => alert('Error'));
 }
 
-function displayResults(input) {
-  input.forEach(i => )
-}
-
-$(function() {
-  handleParkInfo();
-});
-
-
-const extractData = function (data) {
-  data.forEach(//param => {
-    console.log(' ');
-    let {
-      name,
-      description,
-      url,
-      addressUrl,
-    } = //param name;
-  });
+const createTemplate = function(responseJson, maxResults) {
+  $('.parkList').empty();
+  for (let i = 0; (i < responseJson.data.length) & (i < maxResults); i++) {
+    $('.parkList').append(
+      `<li><h1><a href="${responseJson.data[i].url}">${responseJson.data[i].fullName}</a></h1> <p>${responseJson
+        .data[i].description}</p> </li>`
+    );
+    $('.parkList').removeClass('hidden');
+  }
 };
 
-
-
-const createTemplate = function (name, description, url, addressUrl) {
-  let template = `
-  <section>
-    <h2><a href="${url}">${name}</a></h2>
-      <ul>
-        <li>Description: ${description}</li>
-        <li>Address Link: ${addressUrl}</li>
-      </ul>
-  </section>
-  `;
-};
+$(handleParkInfo());
