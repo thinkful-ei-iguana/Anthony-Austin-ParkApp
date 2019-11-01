3e7239b04a88a82d19d2583d0d3fd2b1b239b8cf
AnthonyTB <Anthonylol@protonmail.com>
2019-11-01 12:03:03 -0700

    setup files

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
-$(function() {
-  handleDogBreedType();
-});
+// User will be able to search for parks in one or multiple states
+// User can view multiple results at once
+// Show each park's NAME, DESC., WEBSITE, ADDRESS -> (optional)
 
-function apiCallBreed(breed) {
-  fetch(`https://dog.ceo/api/breed/${breed}/images`)
+function handleParkInfo() {
+  $('#dataEntry').on('submit', e => {
+    e.preventDefault();
+    let parkName = $('.entryPoint').val().toLowerCase();
+    apiCallPark(parkName);
+  });
+}
+
+function apiCallPark(parkName) {
+  fetch('https://developer.nps.gov/api/v1/parks?parkCode=acad&api_key=0nxZI9hF2LSNbDho29iIJk8FXwUmHWzH3HXGrizh')
     .then(response => response.json())
     .then(responseJson => displayResults(responseJson))
     .catch(error => alert('Error'));
 }
+
+function displayResults(input) {}
+
+$(function() {
+  handleParkInfo();
+});
